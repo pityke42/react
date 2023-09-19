c8c6f5d8a7535f5fb283537f627cad955dda717e
pityke <ballllaistvan@gmail.com>
2023-09-19 13:20:05 +0200

    state fundamentals, using useState

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -1,20 +1,28 @@
-
+import { useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+  console.log(step, setStep);
+
+  function handlePervious() {
+    if (step > 1) setStep(step - 1);
+    setStep(step > 1 ? step - 1 : step);
+  }
+  function handleNext() {
+    setStep(step < 3 ? step + 1 : step);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step} : {messages[step - 1]}
@@ -25,6 +33,7 @@ export default function App() {
             backgroundColor: "#7450f2",
             color: "#fff",
           }}
+          onClick={handlePervious}
         >
           Previous
         </button>
@@ -33,6 +42,7 @@ export default function App() {
             backgroundColor: "#7450f2",
             color: "#fff",
           }}
+          onClick={handleNext}
         >
           Next
         </button>
