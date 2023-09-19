9df2cae0020e1efe3ad4a9de61013e77a2b6ae80
pityke <ballllaistvan@gmail.com>
2023-09-19 14:48:20 +0200

    about state

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -7,46 +7,58 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
-  console.log(step, setStep);
+
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePervious() {
-    if (step > 1) setStep(step - 1);
-    setStep(step > 1 ? step - 1 : step);
+    if (step > 1) setStep((currentValue) => currentValue - 1);
+    // setStep(step > 1 ? step - 1 : step);
   }
   function handleNext() {
-    setStep(step < 3 ? step + 1 : step);
+    if (step < 3) setStep((currentValue) => currentValue + 1);
+    // setStep(step < 3 ? step + 1 : step);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{
-            backgroundColor: "#7450f2",
-            color: "#fff",
-          }}
-          onClick={handlePervious}
-        >
-          Previous
-        </button>
-        <button
-          style={{
-            backgroundColor: "#7450f2",
-            color: "#fff",
-          }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button
+        className="close"
+        onClick={() => setIsOpen((currentValue) => !currentValue)}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{
+                backgroundColor: "#7450f2",
+                color: "#fff",
+              }}
+              onClick={handlePervious}
+            >
+              Previous
+            </button>
+            <button
+              style={{
+                backgroundColor: "#7450f2",
+                color: "#fff",
+              }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
